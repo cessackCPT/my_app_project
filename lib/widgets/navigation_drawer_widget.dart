a67ad948b7f39e047b52c275ed0a1386645a5ae4
cessack <cessack@student.wethinkcode.co.za>
2022-03-24 18:55:37 +0200

    adding the route for the leaderboard

--- lib/widgets/navigation_drawer_widget.dart
+++ lib/widgets/navigation_drawer_widget.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app_project/ranking/leaderboard.dart';
 
 class NavigationDrawerWidget extends StatelessWidget{
   final padding = EdgeInsets.symmetric(horizontal: 20);
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
+      child: Material(
         color: Colors.indigo,
         child: ListView(
+          padding: padding,
           children: <Widget>[
             const SizedBox(height: 48),
             buildMenuItem(
               text: 'Leaderboard',
               icon: Icons.leaderboard_outlined,
+              onClicked: () => selectedItem(context, 0),
+            ),
+            const SizedBox(height: 16),
+            buildMenuItem(
+              text: 'Prizes',
+              icon: Icons.monetization_on,
+            ),
+            const SizedBox(height: 16),
+            buildMenuItem(
+              text: 'Questions',
+              icon: Icons.queue,
+            ),
+            const SizedBox(height: 16),
+            buildMenuItem(
+              text: 'Answers',
+              icon: Icons.question_answer_outlined,
             ),
           ],
         ),
@@ -24,13 +42,27 @@ class NavigationDrawerWidget extends StatelessWidget{
     Widget buildMenuItem({
       required String text,
       required IconData icon,
+      //for the links
+      VoidCallback? onClicked,
     }) {
       final color = Colors.white;
+      final hoverColor = Colors.black12;
       return ListTile(
         leading: Icon(icon, color: color),
         title: Text(text,
             style: TextStyle(color: color)),
+        hoverColor: hoverColor,
+        onTap: onClicked,
       );
+    }
 
+    void selectedItem(BuildContext context, int index){
+      switch (index){
+        case 0:
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => Profile(),
+          ));
+          break;
+      }
     }
   }
