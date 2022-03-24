b1be70f1536dfb7fcda60d672cd52fcd023b79a8
cessack <cessack@student.wethinkcode.co.za>
2022-03-24 19:08:54 +0200

    adding the ranking

--- lib/ranking/leaderboard.dart
+++ lib/ranking/leaderboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app_project/widgets/navigation_drawer_widget.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -14,12 +15,14 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: NavigationDrawerWidget(),
       appBar: AppBar(
-        leading: const Icon(Icons.arrow_back),
-        actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.share)),
-          IconButton(onPressed: (){}, icon: const Icon(Icons.person_add)),
-        ],
+        backgroundColor: Colors.indigo,
+        // leading: const Icon(Icons.arrow_back),
+        // actions: [
+        //   IconButton(onPressed: (){}, icon: const Icon(Icons.share)),
+        //   IconButton(onPressed: (){}, icon: const Icon(Icons.person_add)),
+        // ],
         title: const Text( "My Rank", style: TextStyle(fontSize: 29), ),
         centerTitle: true,
         elevation: 0.0,
@@ -29,90 +32,90 @@ class _ProfileState extends State<Profile> {
         child: Column(
           children: [
 
-            Container(
-              padding: const EdgeInsets.only(top : 40),
-              height: 330,
-              decoration: const BoxDecoration(
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(20),
-                      bottomLeft: Radius.circular(20))),
-              child: Column(
-                children: [
-                  Stack(
-                    children: [
-                      const CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            "https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1331&q=80"),
-                        radius: 50,
-                      ),
-                      Positioned(
-                        bottom: 0.0,
-                        right: 0.0,
-                        child: Container(
-                          padding: const EdgeInsets.all(4),
-                          decoration: const BoxDecoration(shape: BoxShape.circle ,color: Colors.white),child: const Icon(Icons.edit,color: Colors.blue,),),
-                      )
-                    ],
-                  ),
-                  const SizedBox( height: 10,
-                  ),
-                  const Text(
-                    "Henry Ford",
-                    style: TextStyle(
-                        fontSize: 22,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Divider(
-                    thickness: 1,
-                    indent: 20,
-                    endIndent: 20,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            "45",
-                            style: TextStyle(
-                                fontSize: 42,
-                                fontWeight: FontWeight.w300,
-                                color: Colors.white.withOpacity(0.9)),
-                          ),
-                          const Text("Level",
-                              style: TextStyle(
-                                  fontSize: 19,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white)),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Text("#335",
-                              style: TextStyle(
-                                  fontSize: 42,
-                                  fontWeight: FontWeight.w300,
-                                  color: Colors.white.withOpacity(0.9))),
-                          const Text("Rank",
-                              style: TextStyle(
-                                  fontSize: 19,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white)),
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
+            // Container(
+            //   padding: const EdgeInsets.only(top : 40),
+            //   height: 330,
+            //   decoration: const BoxDecoration(
+            //       color: Colors.blueGrey,
+            //       borderRadius: BorderRadius.only(
+            //           bottomRight: Radius.circular(20),
+            //           bottomLeft: Radius.circular(20))),
+            //   child: Column(
+            //     children: [
+            //       Stack(
+            //         children: [
+            //           const CircleAvatar(
+            //             backgroundImage: NetworkImage(
+            //                 "https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1331&q=80"),
+            //             radius: 50,
+            //           ),
+            //           Positioned(
+            //             bottom: 0.0,
+            //             right: 0.0,
+            //             child: Container(
+            //               padding: const EdgeInsets.all(4),
+            //               decoration: const BoxDecoration(shape: BoxShape.circle ,color: Colors.white),child: const Icon(Icons.edit,color: Colors.blue,),),
+            //           )
+            //         ],
+            //       ),
+            //       const SizedBox( height: 10,
+            //       ),
+            //       const Text(
+            //         "Henry Ford",
+            //         style: TextStyle(
+            //             fontSize: 22,
+            //             color: Colors.white,
+            //             fontWeight: FontWeight.w500),
+            //       ),
+            //       const SizedBox(
+            //         height: 20,
+            //       ),
+            //       const Divider(
+            //         thickness: 1,
+            //         indent: 20,
+            //         endIndent: 20,
+            //       ),
+            //       const SizedBox(
+            //         height: 20,
+            //       ),
+            //       Row(
+            //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //         children: [
+            //           Column(
+            //             children: [
+            //               Text(
+            //                 "45",
+            //                 style: TextStyle(
+            //                     fontSize: 42,
+            //                     fontWeight: FontWeight.w300,
+            //                     color: Colors.white.withOpacity(0.9)),
+            //               ),
+            //               const Text("Level",
+            //                   style: TextStyle(
+            //                       fontSize: 19,
+            //                       fontWeight: FontWeight.bold,
+            //                       color: Colors.white)),
+            //             ],
+            //           ),
+            //           Column(
+            //             children: [
+            //               Text("#335",
+            //                   style: TextStyle(
+            //                       fontSize: 42,
+            //                       fontWeight: FontWeight.w300,
+            //                       color: Colors.white.withOpacity(0.9))),
+            //               const Text("Rank",
+            //                   style: TextStyle(
+            //                       fontSize: 19,
+            //                       fontWeight: FontWeight.bold,
+            //                       color: Colors.white)),
+            //             ],
+            //           )
+            //         ],
+            //       )
+            //     ],
+            //   ),
+            // ),
             const SizedBox(
               height: 20,
             ),
@@ -145,7 +148,7 @@ class _ProfileState extends State<Profile> {
                             "Rs.${(200000 / (index + 1)).toString().substring(0, 5)}",style: const TextStyle(fontWeight: FontWeight.bold)),
                       );
                     },
-                    separatorBuilder: (context, index) => const Divider(thickness: 1,color: Colors.brown,indent: 10,endIndent: 10,),
+                    separatorBuilder: (context, index) => const Divider(thickness: 1,color: Colors.white,indent: 10,endIndent: 10,),
                     itemCount: 12),
               ),
             )
